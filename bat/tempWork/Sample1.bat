608b45e38c82f22f02699d035cf6e90e3790fe26
n_tanaka <tanaka_narutoshi@ois-yokohama.co.jp>
2018-09-13 13:41:57 +0900

    20180913変更分

--- bat/tempWork/Sample1.bat
+++ bat/tempWork/Sample1.bat
@@ -4,13 +4,17 @@ REM --------------------------------------------------
 SET LHAPLUS="C:\Program Files\Lhaplus\Lhaplus.exe"
 SET TEMP_FOLDER="C:\TempFolder"
 
-SET var1=Hello
-SET var2=World!
+SET VERSION_JP_COUNTER=2030
+SET VERSION_JP_SELF=20230
+SET VERSION_EN_COUNTER=20430
+SET VERSION_EN_SELF=20630
+SET VERSION_EN_SEMISELF=20630
+
 
 pushd %~dp0
     if exist %TEMP_FOLDER% (
-        echo  ERROR: Folder[%TEMP_FOLDER%] exist!
-        exit /b
+      echo  ERROR: Folder[%TEMP_FOLDER%] exist!
+      exit /b
     )
 
     REM 一時フォルダ作成
@@ -18,87 +22,156 @@ pushd %~dp0
 
     REM 国内機
     pushd "01.JP"
-        dir /b
-        echo -----
-
         REM 店頭機
         pushd "国内店頭機\オフラインインストーラ"
-            dir /b
-            echo -----
-
             REM 一般
             pushd "一般\Download"
-                dir /b
-                echo -----
+                REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+                if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
 
-                REM SPK_APP_AC
-                pushd "SPK_APP_AC\2018090621275861"
-                    if exist SPK_APP_AC_2030.zip (
-                        %LHAPLUS% /o:%TEMP_FOLDER%\ SPK_APP_AC_2030.zip
-                        move /Y %TEMP_FOLDER%\SPK_APP_AC_2030\AppFiles AppFiles
-                        rd /s /q %TEMP_FOLDER%\SPK_APP_AC_2030
-                        del SPK_APP_AC_2030.zip
-                    )
-                popd
+                REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+                for /f %%i in ('dir /b /ad') do (
+                    SET zipFolderName=%%i
+                    call :getAppFilesFolder %%i !zipFolderName:JPITR000000001=JPI0!_%VERSION_JP_COUNTER%
+                )
+
+                REM ファイルリストを出力
+                dir /B /S /A-D > "%~dp0\01-01_FileList(JPITR000000001).txt"
             popd
 
             REM キタムラ
-            pushd "キタムラ"
-                dir /b
-                echo -----
+            pushd "キタムラ\Download"
+                REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+                if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
+
+                REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+                for /f %%i in ('dir /b /ad') do (
+                    SET zipFolderName=%%i
+                    call :getAppFilesFolder %%i !zipFolderName:JPITR001000001=JPI1!_%VERSION_JP_COUNTER%
+                )
+
+                REM ファイルリストを出力
+                dir /B /S /A-D > %~dp0\01-02_FileList(JPITR001000001).txt
             popd
 
             REM コイデ
-            pushd "コイデ"
-                dir /b
-                echo -----
-            popd
+            pushd "コイデ\Download"
+                REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+                if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
 
+                REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+                for /f %%i in ('dir /b /ad') do (
+                    SET zipFolderName=%%i
+                    call :getAppFilesFolder %%i !zipFolderName:JPITR015000001=JPI15!_%VERSION_JP_COUNTER%
+                )
+
+                REM ファイルリストを出力
+                dir /B /S /A-D > %~dp0\01-03_FileList(JPITR015000001).txt
+            popd
+        popd
+
+        REM 国内セルフ機
+        pushd "国内セルフ機\JPPSR000000001\x64\Download"
+            REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+            if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
+
+            REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+            for /f %%i in ('dir /b /ad') do (
+                SET zipFolderName=%%i
+                call :getAppFilesFolder %%i !zipFolderName:JPPSR000000001=JPPSR!_%VERSION_JP_SELF%
+            )
+
+            REM ファイルリストを出力
+            dir /B /S /A-D > %~dp0\02_FileList(JPPSR000000001).txt
         popd
     popd
 
+
     REM 海外機
     pushd 02.Global
-        dir /b
-        echo -----
+        REM 海外店頭機
+        pushd "Global_Counter\Download"
+            REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+            if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
+
+            REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+            for /f %%i in ('dir /b /ad') do (
+                SET zipFolderName=%%i
+                call :getAppFilesFolder %%i !zipFolderName:USITR100000001=USI0!_%VERSION_EN_COUNTER%
+            )
+
+            REM ファイルリストを出力
+            dir /B /S /A-D > %~dp0\03_FileList(USITR100000001).txt
+        popd
+
+        REM 海外セルフ機
+        pushd "Global_Self\Download"
+            REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+            if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
+
+            REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+            for /f %%i in ('dir /b /ad') do (
+                SET zipFolderName=%%i
+                call :getAppFilesFolder %%i !zipFolderName:USPSR100000001=USPSR!_%VERSION_EN_SELF%
+            )
+
+            REM ファイルリストを出力
+            dir /B /S /A-D > %~dp0\04_FileList(USPSR100000001).txt
+        popd
+
+        REM 海外セミセルフ機
+        pushd "Global_Self\Download"
+            REM SPK_SYSTEMフォルダにはAppFilesフォルダがないため削除
+            if exist SPK_SYSTEM (rd /s /q SPK_SYSTEM)
+
+            REM 各サブシステムフォルダの解凍とAppFilesフォルダの抽出
+            for /f %%i in ('dir /b /ad') do (
+                SET zipFolderName=%%i
+                call :getAppFilesFolder %%i !zipFolderName:USSSR100000001=USSSR!_%VERSION_EN_SEMISELF%
+            )
+
+            REM ファイルリストを出力
+            dir /B /S /A-D > %~dp0\05_FileList(USSSR100000001).txt
+        popd
     popd
 
     REM 一時フォルダ削除
     rd /s /q %TEMP_FOLDER%
 popd
 
-REM 01.JP\国内セルフ機\JPPSR000000001\x64\Download\SPK_APP_AC\2018090621275861
-REM 01.JP\国内セルフ機\JPPSR000000001\x64\Download\SPK_APP_AC\2018090621275861\SPK_APP_AC_20230\AppFiles
-REM C:\FujifilmApplication\i-Base\SoftUpdate\UpdateWork\SPK_APP_AC\2018090621275861\AppFiles\C_Drive\SmartPrintKiosk\Server\htdocs\clientapps\ac\js\inputImageApp.js
-
 exit /b
 REM --------------------------------------------------
 
 REM --------------------------------------------------------------------------------
-REM AppFilesフォルダからexecutableフォルダへ移動(海外機)
-REM - ※Vup時にAppFiles側へコピーするbatファイルも合わせて作成
+REM Zipを一時フォルダに解凍し、AppFilesを取得
 REM %1: SubSystemフォルダ名
-REM %2: コピー元となるフォルダ("AppFile\C_Drive\"以降を設定)
-REM %3: コピー先に作成するフォルダ名(※コピーbatのファイル名は"copy_%3.bat"となる)
+REM %2: Zipフォルダ名(Version番号付)
 REM --------------------------------------------------------------------------------
-:moveAppFilesToExecutable_EN
-    REM カレントフォルダ設定
-    pushd %~dp0
-
-    REM 共通処理コール(海外機のためOutput_globalフォルダを指定)
-    call :moveAppFilesToExecutable_Common %1 %2 %3 Output_global
-
-    REM 共通処理正常終了時
-    if %errorlevel% == 0 (
-       REM 作業用フォルダからexecutableフォルダにコピー
-       xcopy TEMP_WORK\executable Output_global_AddFiles\Global_Counter\%1\executable  /E /V /I /Y
-       xcopy TEMP_WORK\executable Output_global_AddFiles\Global_Self\%1\executable  /E /V /I /Y
-       xcopy TEMP_WORK\executable Output_global_AddFiles\Global_SemiSelf\%1\executable  /E /V /I /Y
-
-       REM 作業用フォルダの削除
-       rd /s /q TEMP_WORK
+:getAppFilesFolder
+    REM フォルダがない場合、処理を抜ける
+    if not exist %1 (
+        echo NotExist %1
+        exit /b 0
     )
 
+    REM カレントフォルダ設定
+    pushd %1
+        REM カレントフォルダ内のフォルダ名取得
+        for /f %%i in ('dir /b /ad') do (
+            pushd %%i
+                REM Zipファイルが有る場合
+                if exist %2.zip (
+                    REM Zipファイルを一時フォルダに解凍
+                    %LHAPLUS% /o:%TEMP_FOLDER%\ %2.zip
+                    REM 解凍したフォルダからAppFilesフォルダのみ移動
+                    move /Y %TEMP_FOLDER%\%2\AppFiles AppFiles
+                    REM AppFilesフォルダ以外の残骸を削除
+                    rd /s /q %TEMP_FOLDER%\%2
+                    REM Zipファイル削除
+                    del %2.zip
+                )
+            popd
+        )
     REM カレントフォルダ復帰
     popd
 
